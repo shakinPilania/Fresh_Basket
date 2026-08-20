e96bc9548fa5711ba9bffca4ae84ef29a7ea0d88
Shakin <shakin_240585@aitpune.edu.in>
2026-08-20 22:38:10 +0530

    initial :basket

--- src/BasketComponent/OrderSummary.jsx
+++ src/BasketComponent/OrderSummary.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
-
+import React, { useContext } from 'react'
+//! import the context 
+import { CartContext } from '../Context/CartContext'
 function OrderSummary() {
+  const {totalCount , basketData} = useContext(CartContext);
   return (
-    <div>OrderSummary</div>
+    <div>
+      <section className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-10 '>
+        {/* above the top heading  */}
+        <div className='mb-8'>
+          <p className='text-xs text-green-600 font-bold uppercase tracking-widest mb-1 plusJakarta'>Review your order</p>
+          <h1 className='text-3xl font-bold playfair'>Your Basket<span className='ml-3 text-lg font-semibold text-[#78716C]'>({totalCount} items)</span></h1>
+        </div>
+        {/* about the Shop  */}
+        <div className='grid lg:grid-cols-3 gap-8 '>
+          {/* card */}
+          <div className='lg:col-span-2 space-y-3'>
+            {
+              basketData.map((product)=>(
+                <div>
+                  WORKING
+                </div>
+              ))
+            }
+          </div>
+          {/* order summary  */}
+          <div>WORKING</div>
+        </div>
+      </section>
+    </div>
   )
 }
 
