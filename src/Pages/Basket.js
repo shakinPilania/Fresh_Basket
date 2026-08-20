22ece1f0419edb717be4062d34c8378f3f062868
Shakin <shakin_240585@aitpune.edu.in>
2026-08-20 17:52:51 +0530

    Initial Baske :Create Context

--- src/Pages/Basket.js
+++ src/Pages/Basket.js
@@ -4,13 +4,14 @@ import {CartContext} from "../Context/CartContext"
 
 //! import the component 
 import Empty from '../BasketComponent/Empty'
+import OrderSummary from '../BasketComponent/OrderSummary';
 
 function Basket() {
   const {basketData} = useContext(CartContext);
   return (
     <div>
       {
-        basketData.length==0?<Empty/>:(<p>WORKING</p>)
+        basketData.length==0?<Empty/>:(<OrderSummary/>)
       }
       
     </div>
